d7adf1bf48ab62e94e75cc43b410e861ea5b65d0
Mana Mehraein <35238795+manamhr@users.noreply.github.com>
2020-03-18 13:54:52 -0700

    Add files via upload

---
+++ HW6Submit/ITPDoubleList.hpp
@@ -0,0 +1,637 @@
+// ITP 365 Fall 2017
+// HW6 - Doubly Linked List and Movie Player
+// Name: Mana Mehraein
+// Email: mehraein@usc.edu
+// Platform: Mac
+//
+// ITPDoubleList.hpp
+// Defines and implements the ITPDoubleList collection
+//
+
+#pragma once
+#include "error.h"
+#include <ostream>
+#include <sstream>
+#include "leaker.h"
+
+template <typename T>
+class ITPDoubleList
+{
+private:
+    
+    // Node only used internally to list
+    struct Node
+    {
+        // Data in this node
+        T mData;
+        // Link to previous node
+        Node* mPrev;
+        // Link to next node
+        Node* mNext;
+    };
+    
+    // Pointer to the head node
+    Node* mHead;
+    // Pointer to the tail node
+    Node* mTail;
+    // Number of nodes currently in the list
+    unsigned mSize;
+    
+public:
+    // Iterator helper class...
+    class Iterator
+    {
+    private:
+        Node* mCurr;
+    public:
+        // Needed for List access
+        friend class ITPDoubleList;
+        // Constructors
+        Iterator()
+        {
+            // TODO: Implement
+            mCurr = nullptr;
+            
+        }
+        
+        Iterator(Node* inCurr)
+        {
+            // TODO: Implement
+            mCurr=inCurr;
+        }
+        
+        Iterator(const Iterator& src)
+        {
+            // TODO: Implement
+            mCurr=src.mCurr;
+            
+        }
+        
+        // Assignment
+        Iterator& operator=(const Iterator& src)
+        {
+            // TODO: Implement
+            mCurr=src.mCurr;
+            
+            return (*this); // FIX RETURN VALUE
+        }
+        
+        // Incrementors / Decrementors
+        // ++i
+        Iterator& operator++()
+        {
+            // TODO: Implement
+            
+            mCurr=mCurr->mNext;
+            
+            return (*this); // FIX RETURN VALUE
+        }
+        
+        // i++
+        Iterator operator++(int)
+        {
+            // TODO: Implement
+            Iterator retval(*this);
+            mCurr=mCurr->mNext;
+            return (retval); // FIX RETURN VALUE
+        }
+        
+        // --i
+        Iterator& operator--()
+        {
+            // TODO: Implement
+            mCurr=mCurr->mPrev;
+            return (*this); // FIX RETURN VALUE
+        }
+        
+        // i--
+        Iterator operator--(int)
+        {
+            // TODO: Implement
+            Iterator retval(*this);
+            mCurr=mCurr->mPrev;
+            return (retval); // FIX RETURN VALUE
+        }
+        
+        // Dereference
+        T& operator*() const
+        {
+            // TODO: Implement
+            return mCurr->mData; // FIX RETURN VALUE
+        }
+        
+        Node* operator->() const
+        {
+            // TODO: Implement
+            return mCurr; // FIX RETURN VALUE
+        }
+        
+        // Comparison
+        bool operator==(const Iterator& rhs) const
+        {
+            // TODO: Implement
+            
+            return mCurr == rhs.mCurr; // FIX RETURN VALUE
+        }
+        
+        bool operator!=(const Iterator& rhs) const
+        {
+            // TODO: Implement
+            
+            return (mCurr !=rhs.mCurr); // FIX RETURN VALUE
+        }
+        
+    };
+    
+    // List functions...
+    
+    // Function: Default Constructor
+    // Purpose: Initializes size to 0 and head/tail to null
+    // Input: None
+    // Returns: Nothing
+    ITPDoubleList()
+    {
+        mSize = 0;
+        mHead = nullptr;
+        mTail = nullptr;
+    }
+    
+    // Function: Copy Constructor
+    // Purpose: Initializes this list as a deep copy of other
+    // Input: Other list to copy (const reference)
+    // Returns: Nothing
+    ITPDoubleList(const ITPDoubleList& other)
+    {
+        // TODO: Implement
+        
+        //        1. Set size to 0, head to null, tail to null
+        mSize=0;
+        mHead=nullptr;
+        mTail=nullptr;
+        //        2. Create a temp that points to the head node in “other”
+        Node* temp=other.mHead;
+        //        3. While temp is not null…
+        //        1. Insert_back temp’s data into this list
+        //        2. Advance temp to the next node
+        while (temp!=nullptr)
+        {
+            push_back(temp->mData);
+            temp=temp->mNext;
+        }
+        
+    }
+    
+    // Function: Destructor
+    // Purpose: Calls the clear function to delete the list
+    // Input: None
+    // Returns: Nothing
+    ~ITPDoubleList()
+    {
+        // TODO: Implement
+        clear();
+    }
+    
+    // Function: clear
+    // Purpose: Deletes every node in this list, sets size to 0, and head/tail to null
+    // Input: None
+    // Returns: Nothing
+    void clear()
+    {
+        // TODO: Implement
+        
+        //        a) Call remove_front
+        //        1. While size > 0…
+        while (mSize!=0)
+        {
+            pop_front();
+        }
+        
+    }
+    
+    // Assignment Operator
+    // Function: Assignment Operator
+    // Purpose: Clear this list, and then make a deep copy of other
+    // Input: Other list to copy (const reference)
+    // Returns: *this
+    ITPDoubleList& operator=(const ITPDoubleList& other)
+    {
+        //            1. Call clear (to clear out any values already in this list)
+        clear();
+        //            2. Create a temp that points to the head node in “other”
+        Node* temp= other.mHead;
+        //            3. While temp is not null…
+        //                1. Insert_back temp’s data into this list
+        //                2. Advance temp to the next node
+        while(temp!=nullptr)
+        {
+            push_back(temp->mData);
+            temp=temp->mNext;
+        }
+        //            4. By convention, operator= returns *this
+        
+        
+        return (*this); // FIX RETURN VALUE
+    }
+    
+    // Function: size
+    // Purpose: Gets the current size of the linked list
+    // Input: None
+    // Returns: size of the list
+    unsigned size() const
+    {
+        // TODO: Implement
+        return mSize; // FIX RETURN VALUE
+    }
+    
+    // Function: push_front
+    // Purpose: Inserts a node at the front of the linked list
+    // Input: Value to store in the new node
+    // Returns: Nothing
+    void push_front(const T& value)
+    {
+        // TODO: Implement
+        //        1. Dynamically allocate a new node
+        
+        Node* mNewNode= new Node();
+        
+        //        a) Set its data to the value
+        mNewNode -> mData= value;
+        //        b) Set its previous to null (since this will be the new head)
+        mNewNode -> mPrev = nullptr;
+        //        c) Set its next to the current head
+        mNewNode -> mNext = mHead;
+        //        2. If the size is 0…The tail is the new node
+        if (mSize==0)
+        {
+            mTail= mNewNode;
+        }
+        //        3. Otherwise…set the previous of the current head to the new node
+        else
+        {
+            mHead -> mPrev = mNewNode;
+        }
+        //        4. Set the head to the new node
+        mHead = mNewNode;
+        //        5. Increment the size
+        mSize++;
+        
+    }
+    
+    // Function: front
+    // Purpose: Gets the value of the node at the front of the list
+    // Input: None
+    // Returns: Value of the node at the front of the list
+    T& front()
+    {
+        // TODO: Implement
+        
+        //        1. If the size is 0, error!
+        
+        if (mSize==0)
+        {
+            error("Size is 0!!");
+        }
+        //        2. Return the data at the head node
+        
+        return (mHead->mData); // FIX RETURN VALUE
+    }
+    
+    // Function: pop_front
+    // Purpose: Removes the node at the front of the list
+    // Input: None
+    // Returns: None
+    void pop_front()
+    {
+        // TODO: Implement
+        //        1. If the size is 0…error!
+        if (mSize==0)
+        {
+            error("Size is 0!!");
+        }
+        //        2. Otherwise if the size is 1…
+        //            1. Delete the head node
+        //            2. Set head and tail to null, size to 0
+        
+        else if (mSize==1)
+        {
+            delete mHead;
+            mHead= nullptr;
+            mTail= nullptr;
+            mSize=0;
+        }
+        //        3. Otherwise…
+        //            1. Set a temp equal to the next node after head
+        //            2. Delete the head node
+        //            3. The new head is temp
+        //            4. The new head previous is now null
+        //            5. Decrement size
+        else
+        {
+            Node* temp=(mHead->mNext);
+            delete mHead;
+            mHead=temp;
+            mHead->mPrev=nullptr;
+            mSize--;
+        }
+        
+    }
+    
+    // Function: push_back
+    // Purpose: Inserts a node at the end of the linked list
+    // Input: Value to store in the new node
+    // Returns: Nothing
+    void push_back(const T& value)
+    {
+        // TODO: Implement
+        //        1. Dynamically allocate a new node
+        Node* mNewNode= new Node();
+        
+        //        a) Set its data to the value
+        
+        mNewNode -> mData= value;
+        
+        //        b) Set its previous to the current tail
+        mNewNode -> mPrev = mTail;
+        
+        //        c) Set the next to null (since this will be the new tail)
+        mNewNode -> mNext = nullptr;
+        
+        //        2. If the size is 0…The head is the new node
+        if (mSize == 0)
+        {
+            mHead=mNewNode;
+        }
+        //        3. Otherwise…set the next of the current tail to the new node
+        else
+        {
+            mTail-> mNext=mNewNode;
+        }
+        //        4. Set the tail to the new node
+        mTail=mNewNode;
+        //        5. Increment the size
+        mSize++;
+        
+    }
+    
+    // Function: back
+    // Purpose: Gets the value of the node at the back of the list
+    // Input: None
+    // Returns: Value of the node at the back of the list
+    T& back()
+    {
+        // TODO: Implement
+        //        1. If the size is 0, error!
+        if (mSize==0)
+        {
+            error("Size is 0!");
+        }
+        //        2. Return the data at the tail node
+        
+        return (mTail->mData); // FIX RETURN VALUE
+    }
+    
+    // Function: pop_back
+    // Purpose: Removes the node at the end of the list
+    // Input: None
+    // Returns: None
+    void pop_back()
+    {
+        // TODO: Implement
+        //        1. If the size is 0…error!
+        if (mSize==0)
+        {
+            error("Size is 0!");
+        }
+        //        2. Otherwise if the size is 1…
+        //          1. Delete the tail node
+        //          2. Set head and tail to null, size to 0
+        else if (mSize==1)
+        {
+            delete mTail;
+            mHead=nullptr;
+            mTail=nullptr;
+            mSize=0;
+        }
+        //
+        //        3. Otherwise…
+        //            1. Set a temp equal to the previous node before the tail
+        //            2. Delete the tail node
+        //            3. The new tail is temp
+        //            4. The new tail next is now null
+        //            5. Decrement size
+        
+        else
+        {
+            Node* temp= mTail->mPrev;
+            delete mTail;
+            mTail=temp;
+            mTail->mNext=nullptr;
+            mSize--;
+        }
+        
+    }
+    
+    // Function: begin
+    // Purpose: Gets an iterator for the start of the list
+    // Input: None
+    // Returns: Iterator pointing to beginning of list
+    Iterator begin() const
+    {
+        // TODO: Implement
+        Iterator my_iterator=Iterator(mHead);
+        return my_iterator;
+        // FIX RETURN VALUE
+    }
+    
+    // Function: end
+    // Purpose: Gets an iterator for the end of the list
+    // Input: None
+    // Returns: Iterator pointing to end of list
+    Iterator end() const
+    {
+        // TODO: Implement
+//        Iterator my_iterator2=Iterator(mTail);
+        return Iterator(mTail->mNext); // FIX RETURN VALUE
+    }
+    
+    // Function: erase
+    // Purpose: Removes the item at the node pointed to by i
+    // Input: Iterator pointing to a valid node to remove before
+    // Returns: A new iterator pointing to the node after the removed item
+    Iterator erase(Iterator& i)
+    {
+        // TODO: Implement
+        
+        //        1. If the iterator is invalid … error!
+        if (i.mCurr==nullptr)
+        {
+            error("Nothing to search!!");
+            return nullptr;
+        }
+        //        2. If the iterator is at begin
+        //            1. pop_front
+        //            2. Return begin
+        else if (i.mCurr==mHead)
+        {
+            pop_front();
+            return mHead;
+        }
+        
+        //        3. If the iterator is at the tail
+        //            1. pop_back
+        //            2. Return end
+        
+        else if (i.mCurr==mTail)
+        {
+            pop_back();
+            return mTail;
+            
+        }
+        //        4. Otherwise…
+        //            1. Make “before”, “after”, and “toDel” pointers
+        //            2. Delete “toDel”
+        //            3. Set before’s next to after
+        //            4. Set after’s previous to before
+        //            5. Decrement size
+        //            6. Return an iterator to after
+        else
+        {
+            Node* before=i.mCurr->mPrev;
+            Node* after=i.mCurr->mNext;
+            Node* toDel=i.mCurr;
+            delete toDel;
+            before->mNext=after;
+            after->mPrev=before;
+            mSize--;
+            return after;
+            
+        }
+//        return *(new Iterator); // FIX RETURN VALUE
+    }
+    
+    // Function: insert
+    // Purpose: Inserts a list item before node pointed to by i
+    // Input: Iterator pointing to a valid node to insert before
+    // Returns: A new iterator pointing to the inserted value
+    Iterator insert(Iterator& i, const T& val)
+    {
+        // TODO: Implement
+        //        1. If the iterator is invalid … error!
+        if (i.mCurr==nullptr)
+        {
+            error("Nothing to search!!");
+            return nullptr;
+        }
+        //        2. If the iterator is at begin
+        //            1. push_front
+        //            2. Return begin
+        else if (i.mCurr==mHead)
+        {
+            push_front(val);
+            return mHead;
+        }
+        //        3. If the iterator is at end
+        //            1. push_back
+        //            2. Return iterator to last element (not end!)
+        
+        else if (i.mCurr==mTail->mNext)
+        {
+            push_back(val);
+            return mTail;
+        }
+        //        4. Otherwise…
+        //            1. Make “before” and “after” pointers
+        //            2. Dynamically allocate a new node
+        //                a) Set its data to the value
+        //                b) Set its previous to before
+        //                c) Set its next to after
+        //            3. Set before’s next to the new node
+        //            4. Set after’s previous to the new node
+        //            5. Increment size
+        //            6. Return an iterator pointing to the inserted node
+        
+        else
+        {
+            Node* before=i.mCurr->mPrev;
+            Node* after=i.mCurr;
+            Node* mNewNode= new Node;
+            mNewNode->mData=val;
+            mNewNode->mPrev=before;
+            mNewNode->mNext=after;
+            before->mNext=mNewNode;
+            after->mPrev=mNewNode;
+            mSize++;
+            return mNewNode;
+        }
+        
+        
+        
+    }
+    
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // !!!! DO NOT EDIT BELOW THIS LINE !!!!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+public:
+    // Function: Output Stream Operator
+    // Purpose: Prints the contents of the list to the provided stream
+    // Written in the format of {x, y, ..., z}
+    // Input: Stream to write to, list to write
+    // Output: Stream after values are written
+    friend std::ostream& operator<<(std::ostream& os, const ITPDoubleList<T>& list)
+    {
+        os << "{";
+        
+        Node* temp = list.mHead;
+        while (temp != nullptr)
+        {
+            os << temp->mData;
+            if (temp != list.mTail)
+            {
+                os << ",";
+            }
+            
+            temp = temp->mNext;
+        }
+        
+        os << "}";
+        return os;
+    }
+    
+    // Function: toString
+    // Purpose: Converts list to a string
+    // Input: None
+    // Output: String (in the format as the ostream)
+    std::string toString() const
+    {
+        std::stringstream str;
+        str << *this;
+        return str.str();
+    }
+    
+    // Function: toReverseString
+    // Purpose: Outputs the list in reverse
+    // Input: None
+    // Output: String in reverse
+    std::string toReverseString() const
+    {
+        std::stringstream str;
+        
+        str << "{";
+        
+        Node* temp = mTail;
+        while (temp != nullptr)
+        {
+            str << temp->mData;
+            if (temp != mHead)
+            {
+                str << ",";
+            }
+            
+            temp = temp->mPrev;
+        }
+        
+        str << "}";
+        
+        return str.str();
+    }
+};
---
+++ HW6Submit/MoviePlayer.cpp
@@ -0,0 +1,282 @@
+#include "MoviePlayer.h"
+
+// ITP 365 Fall 2017
+// HW6 - Doubly Linked List and Movie Player
+// Name: Mana Mehraein
+// Email: mehraein@usc.edu
+// Platform: Mac
+
+
+
+// Function: Parameterized Constructor
+// Purpose: Sets member variables to input, clears list, and calls loader
+// Input: None
+// Returns: Nothing
+MoviePlayer::MoviePlayer(std::string filmName)
+{
+    // TODO: Implement
+    //    The parameterized constructor should set the appropriate
+    //    member variables, ensure that the tape is empty, then call the member function
+    //    loadTape on the inputted file name.
+    
+    // Holds the current frame the film is on
+    currFrameNum=0;
+    
+    // Stores the name of the input file
+    // that has the ASCII film in it
+    mFilmName=filmName;
+    
+    // The film is made a series of strings
+    // each string is 1 frame to be displayed
+    // by the main function
+    // Stores the current position of the reader
+//    mFilm.clear();
+    loadTape();
+    rewind();
+    
+}
+
+// Function: Destructor
+// Purpose: Empties the list
+// Input: None
+// Returns: Nothing
+MoviePlayer::~MoviePlayer()
+{
+    // TODO: Implement
+    //    While not necessary, we’ll protectively ensure that the tape is
+    //    empty here.
+    mFilmName.clear();
+}
+
+// Function: goFwrd
+// Purpose: Moves the current "frame" forward by 1 frame
+// Also keeps track of how many frames have been viewed
+// Shouldn't go past the end of the movie
+// Input: None
+// Returns: Nothing
+void MoviePlayer::goFwrd()
+{
+    // TODO: Implement
+    
+    //    This function should move the tape forward 1 frame. It should also
+    //    increment the currFrameNum counter.
+    
+    if (pos->mNext!=nullptr)
+    {
+        currFrameNum++;
+        pos++;
+    }
+    
+}
+
+// Function: goBack
+// Purpose: Moves the current "frame" backward by 1 frame
+// Also keeps track of how many frames have been viewed
+// Shouldn't go past the end of the movie
+// Input: None
+// Returns: Nothing
+void MoviePlayer::goBack()
+{
+    // TODO: Implement
+    
+    //    This function should move the tape backward 1 frame. It should
+    //    also decrement the currFrameNum counter.
+    if (pos->mPrev!=nullptr)
+    {
+        currFrameNum--;
+        pos--;
+    }
+}
+
+// Function: rewind
+// Purpose: Moves the current "frame" to the movie's beginning again
+// Input: None
+// Returns: Nothing
+void MoviePlayer::rewind()
+{
+    // TODO: Implement
+    //    Here you’ll start the tape over again, being sure to keep track of
+    //    the currFrameNum counter.
+    currFrameNum=1;
+    pos=mFilm.begin();
+}
+
+// Function: getCurrFrame
+// Purpose: Gets the string that contains the current "frame" as stored
+// in the list "mFilm"
+// Input: None
+// Returns: String with the current "frame"
+std::string MoviePlayer::getCurrFrame()
+{
+    // TODO: Implement
+    //    Here you’ll return the current currFrameNum counter.
+    
+    return *pos; // FIX RETURN VALUE
+}
+
+// Function: delCurrFrame
+// Purpose: Deletes the current frame, moves forward to the next one
+// Also keeps track of how many frames have been viewed
+// Input: None
+// Returns: Nothing
+void MoviePlayer::delCurrFrame()
+{
+    // TODO: Implement
+    //    This function will delete the current frame and move the tape forward 1 frame.
+    if (pos->mPrev!=nullptr)
+    {
+    pos= mFilm.erase(pos); ////check this
+}
+}
+
+// Function: copyCurrFrame
+// Purpose: Copies current frame and places the copy BEFORE the current frame
+// Also keeps track of how many frames have been viewed
+// Input: None
+// Returns: Nothing
+void MoviePlayer::copyCurrFrame()
+{
+    // TODO: Implement
+    
+    //    This will copy the current frame and move the tape to the newly added frame.
+    pos=mFilm.insert(pos,*pos); //check
+
+}
+
+// Function: getCurrFrameNum
+// Purpose: Gets the "index" that corresponds to the current frame
+// Input: None
+// Returns: Number of frames that have been viewed
+unsigned MoviePlayer::getCurrFrameNum() const
+{
+    // TODO: Implement
+    //    One takes in no input and returns a string with the contents of the current frame.
+    return currFrameNum; // FIX RETURN VALUE
+}
+
+// Function: getNumFrames
+// Purpose: Gets the total number of frames in the movie
+// Input: None
+// Returns: The overall number of frames in the movie
+unsigned int MoviePlayer::getNumFrames() const
+{
+    // TODO: Implement
+    // Here you’ll return the total number of frames in the movie.
+    return mFilm.size(); // FIX RETURN VALUE
+}
+
+// Function: getFrameSize
+// Purpose: Gives the number of lines in a frame
+// For how big the "screen" for the film should be
+// Input: None
+// Returns: The value stored in FRAMESIZE
+unsigned int MoviePlayer::getFrameSize() const
+{
+    // TODO: Implement
+    // This function gets the number of line in a frame.
+    return FRAMESIZE; // FIX RETURN VALUE
+}
+
+// Function: getCurrFrame
+// Purpose: Retrieves the current "frame" from the film list
+// Modifies the inputted vector of GLabels to contain the current frame
+// Input: A vector of "GLabels" that are the "screen" -- passed by reference
+// Returns: Nothing
+void MoviePlayer::getCurrFrame(Vector<GLabel*>& screen)
+{
+    // TODO: Implement
+    // takes in a Vector of GLabel pointers, passed in by reference.
+    // There should be 13 items in the Vector which is the same as the number of lines in each frame.
+    // Each GLabel must have its label set to the corresponding line of text in the frame.
+    //
+    // do get line on current frame
+    // check on how to change labels on stanford
+    //make a new string stream
+    //set up from current frame in there
+    
+    
+    // or instead of *pos I could use getCurrFrame()
+    std::stringstream frames(*pos);
+    
+    for (int i=0; i< FRAMESIZE; i++)
+    {
+        //get one line
+        std::string num;
+        getline(frames,num);
+        //        std::string num  = getCurrFrame();
+        screen[i]->setLabel(num);
+    }
+    
+}
+
+// Function: loadTape
+// Purpose: Uses mFilmName (set by constructor) to fill
+// mFilm with strings that make up the movie to display
+// Input: None
+// Returns: Nothing
+void MoviePlayer::loadTape()
+{
+    // TODO: Implement
+    
+    //    This function should open the inputted text file. Assume the
+    //    user only enters one of the “.ani” files. This function parses out the text file and makes
+    //    up the frames and fills the ITPDoubleList with the animations.
+    //        std::ifstream mFilmName("starwar.ani");
+    std::ifstream file (mFilmName);
+    if (file.is_open())
+    {
+        while (!file.eof())
+        {
+            std::string line;
+            std::getline(file, line);
+            //            auto n= line.substr(0,2);
+            //            std::cout << n<< std::endl;
+            //            auto number=stringToInteger(n);
+            //            std::cout << number<< std::endl;
+            
+            //use stanford, use stringstream
+            if (!stringIsInteger(line))
+            {
+                break;
+            }
+            else
+            {
+                //if not a number, break
+                //if it is a number save it for later
+                //read in 13 lines
+                //smush them all together
+                //insert the smush n number of times
+                
+                std::string page;
+                std::string frame="";
+                
+               
+                    for (int i=0; i<13; i++)
+                    {
+                        std::getline(file, page);
+                        frame+=page;
+                        frame=frame+" \n";
+                    }
+                    //                    std::cout << frame << std::endl;
+                    
+                    //converto line to int
+                    if(stringIsInteger(line))
+                    {
+                        int number=stringToInteger(line);
+                        for (int i=0; i<number; i++)
+                        {
+                            mFilm.push_back(frame);
+                        }
+                    }
+                    
+                
+            }
+            
+        }
+        
+    }
+    else
+    {
+        error ("Sorry file does not exist!");
+    }
+}
